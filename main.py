cb5bfb6e672dbb7b1011f87a5ab96ab13b09683a
aryan <kanadaaryan9@gmail.com>
2024-12-24 21:31:22 +0530

    Added CreditCard class and check its validation with cards.csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df = pd.read_csv("hotel.csv")
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -35,14 +36,33 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, cvc, holder):
+        card_data = {"number": self.number, "expiration": expiration, "cvc": cvc, "holder": holder}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 Id = int(input("Enter the id of Hotel: "))
 hotel = Hotel(Id)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_number = input("Enter Credit card number(1234): ")
+    credit_card = CreditCard(number=credit_number)
+
+    if credit_card.validate(expiration="12/26", cvc="123", holder="JOHN SMITH"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+         print("Credit card not vaild")
+
 else:
     print("Hotel is not free")
