fd998d8c2452090211685b5d3bd62fb62cb14887
aryan <kanadaaryan9@gmail.com>
2025-01-03 14:37:48 +0530

    def __eq__

--- main2.py
+++ main2.py
@@ -28,6 +28,12 @@ class Hotel:
     def get_hotel_count(cls,data):
         return len(data)
 
+    def __eq__(self, other):
+        if self.hotel_id == other.hotel_id:
+            return True
+        else:
+            return False
+
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
